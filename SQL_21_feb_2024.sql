db8169c0714cf0c1ce79d4e909ed71b12bc7e0f4
RedTearStone <148710224+EliShtarker@users.noreply.github.com>
2024-02-22 09:26:39 +0200

    Update SQL_21_feb_2024.sql

--- SQL_21_feb_2024.sql
+++ SQL_21_feb_2024.sql
@@ -1,11 +1,11 @@
  CREATE TABLE students (id INTEGER PRIMARY Key, name TEXT, city TEXT, birth 
 INTEGER);
 
-INSERT INTO shopping VALUES (1, 'shalom', 'tel aviv , 1974);
-INSERT INTO shopping VALUES (2, 'yuri', 'raanana, 1980);
-INSERT INTO shopping VALUES (3, 'anat', 'rishon, 1994);
-INSERT INTO shopping VALUES (4, 'dana', 'rehovot', 1990);
-INSERT INTO shopping VALUES (5, 'omer', 'jerusalem, 1987);
+INSERT INTO students VALUES (1, 'shalom', 'tel aviv' , 1974);
+INSERT INTO students VALUES (2, 'yuri', 'raanana', 1980);
+INSERT INTO students VALUES (3, 'anat', 'rishon', 1994);
+INSERT INTO students VALUES (4, 'dana', 'rehovot', 1990);
+INSERT INTO students VALUES (5, 'omer', 'jerusalem', 1987);
 
 
 CREATE TABLE grade (id INTEGER PRIMARY Key,grade INTEGER);
